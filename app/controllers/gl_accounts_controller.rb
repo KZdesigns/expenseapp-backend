d5feb103d5f7d8bf0ede302d563bd21703c99f8f
Kyle Zimmerman <kzdesignsios@gmail.com>
2022-09-16 16:49:24 -0700

    added update functionality for GL accounts

--- app/controllers/gl_accounts_controller.rb
+++ app/controllers/gl_accounts_controller.rb
@@ -5,8 +5,7 @@ class GlAccountsController < ApplicationController
     #     @gl_accounts = GlAccount.all
     #     render json: @gl_accounts
     # end
-    
-    
+
     def create
         @gl_account = GlAccount.new(gl_accounts_params)
         if @gl_account.save
@@ -16,10 +15,20 @@ class GlAccountsController < ApplicationController
         end
     end
 
+    def update
+        @gl_account = GlAccount.find(gl_accounts_params[:id])
+
+        if @gl_account.update(gl_accounts_params)
+            render json: @gl_account
+        else
+            render json: @gl_accounts.erros, status: :unprocessable_entity
+        end
+    end
+
     private
 
     def gl_accounts_params
-        params.require(:gl_account).permit(:number, :name, :gl_type)
+        params.require(:gl_account).permit(:id, :number, :name, :gl_type)
     end
 
 end
